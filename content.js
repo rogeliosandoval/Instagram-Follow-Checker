735fec7c5f71c06ea127a1584fed06e85820ff25
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2025-09-08 23:15:46 -0500

    update manifest and content

--- content.js
+++ content.js
@@ -1,5 +1,4 @@
-// Right now this file doesn’t need to do much.
-// You can expand later if you want auto-scrolling or background scraping.
+// Right now this file doesn’t need to do much. This is just for what happens when you visit Instagram
 console.log('Instagram scraper content script loaded.')
 localStorage.removeItem('scrapedFollowers')
 localStorage.removeItem('scrapedFollowing')
